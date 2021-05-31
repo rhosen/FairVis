042861a41f3f6a8aa3fcb1484de9b3e5b2633edc
Rubel Hosen <rubelhosen.cs@gmail.com>
2021-06-01 01:09:13 +0600

    Updates

--- precache-manifest.44fdd4fdc89c6355962a632362914fb0.js
+++ precache-manifest.44fdd4fdc89c6355962a632362914fb0.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6378d5d3622b3bcf5582e0f107bfe7cc",
+    "revision": "cb6c79cd6e4fbcd7097a60be92e3e0ea",
     "url": "/FairVis/index.html"
   },
   {
-    "revision": "c133db76854486ff6b65",
+    "revision": "03dca8645f39e52a4985",
     "url": "/FairVis/static/css/main.ab4d9660.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/FairVis/static/js/2.4ac1dffe.chunk.js"
   },
   {
-    "revision": "c133db76854486ff6b65",
-    "url": "/FairVis/static/js/main.9c77192b.chunk.js"
+    "revision": "03dca8645f39e52a4985",
+    "url": "/FairVis/static/js/main.e24f8710.chunk.js"
   },
   {
     "revision": "34730e01619f251436ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FairVis/precache-manifest.aef7635f1ca6c107c1314f4213216d50.js"
+  "/FairVis/precache-manifest.44fdd4fdc89c6355962a632362914fb0.js"
 );
 
 self.addEventListener('message', (event) => {
